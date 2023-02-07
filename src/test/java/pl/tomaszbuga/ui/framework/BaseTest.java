bc398e4985f6df158480be38743bdd53cca020c1
tomasz.buga <tomasz.buga@tset.com>
2023-02-07 22:42:02 +0100

    added headless mode, for CI/CD purposes

--- src/test/java/pl/tomaszbuga/ui/framework/BaseTest.java
+++ src/test/java/pl/tomaszbuga/ui/framework/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -22,9 +23,20 @@ public abstract class BaseTest {
     }
 
     private void setupChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        options.addArguments(
+                "--headless",
+                "--disable-gpu",
+                "--window-size=1920,1200",
+                "--ignore-certificate-errors",
+                "--disable-extensions",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+        );
+
+        driver = new ChromeDriver(options);
     }
 
     protected WebDriver getDriver() {
