52a68cd35e7d8aa1d9332752bc819dccaf2ba193
tomasz.buga <tomasz.buga@tset.com>
2023-02-09 13:29:56 +0100

    cleanup of the gh-pages branch

--- src/test/java/pl/tomaszbuga/ui/utils/UserUtils.java
+++
@@ -1,9 +0,0 @@
-package pl.tomaszbuga.ui.utils;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class UserUtils {
-    public static final String username = System.getProperty("email");
-    public static final String pass = System.getProperty("password");
-}
